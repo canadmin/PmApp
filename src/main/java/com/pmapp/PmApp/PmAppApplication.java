000970a818d673ddc08b9dd90fdd0a515ba2cbbd
canadmin <icanyard@gmail.com>
2019-04-13 12:18:54 +0300

    pmapp

---
+++ src/main/java/com/pmapp/PmApp/PmAppApplication.java
@@ -0,0 +1,13 @@
+package com.pmapp.PmApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PmAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PmAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pmapp/PmApp/PmAppApplicationTests.java
@@ -0,0 +1,16 @@
+package com.pmapp.PmApp;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PmAppApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
